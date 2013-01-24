8f83854ea273171d519c1fe7c6d33854c4b08335
Juhyeong Yu <zzurang@hotmail.com>
2013-01-24 01:21:17 -0800

    testing heroku deployment

--- server.js
+++ server.js
@@ -10,9 +10,18 @@ var express = require('express'),
     dust = require('dustjs-linkedin'),
     cons = require('consolidate');
 
+// var app = express(),
+//     server = app.listen(3000),
+//     io = require('socket.io').listen(server);
+
 var app = express(),
     server = app.listen(3000),
     io = require('socket.io').listen(server);
+
+io.configure(function () { 
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 30); 
+});
 
 app.configure(function() {
   app.engine('dust', cons.dust);
@@ -53,68 +62,66 @@ app.get('/signup', user.new);
 /**
  * AMQP Messaging prototype
  */
-var connection = amqp.createConnection({
-  host: '127.0.0.1'
-});
+// var connection = amqp.createConnection({
+//   host: '127.0.0.1'
+// });
 
 
-var emailToRenderEventsMap = {};
-connection.addListener('ready', function() {
-  var exchange1 = connection.exchange('ui-service', {
-    type: 'fanout'
-  });
-  var exchange2 = connection.exchange('auth-service', {
-    type: 'fanout'
-  });
-  var i = 0;
-  var q = connection.queue('ui-front-shared-queue', function() {
-    q.bind(exchange1, "*");
-    q.bind(exchange2, "*");
-    q.subscribe(function(m) {
-      data = JSON.parse(m.data.toString())
-      console.log(emailToRenderEventsMap)
-      if (data.email) {
-        if (emailToRenderEventsMap[data.email] != undefined) {
-          emailToRenderEventsMap[data.email].push((data.email + " -> " + i++));
-        } else {
-          emailToRenderEventsMap[data.email] = [data.email + " -> " + i++];
-        }
-      }
-    })
-  });
-});
+// var emailToRenderEventsMap = {};
+// connection.addListener('ready', function() {
+//   var exchange1 = connection.exchange('ui-service', {
+//     type: 'fanout'
+//   });
+//   var exchange2 = connection.exchange('auth-service', {
+//     type: 'fanout'
+//   });
+//   var i = 0;
+//   var q = connection.queue('ui-front-shared-queue', function() {
+//     q.bind(exchange1, "*");
+//     q.bind(exchange2, "*");
+//     q.subscribe(function(m) {
+//       data = JSON.parse(m.data.toString())
+//       console.log(emailToRenderEventsMap)
+//       if (data.email) {
+//         if (emailToRenderEventsMap[data.email] != undefined) {
+//           emailToRenderEventsMap[data.email].push((data.email + " -> " + i++));
+//         } else {
+//           emailToRenderEventsMap[data.email] = [data.email + " -> " + i++];
+//         }
+//       }
+//     })
+//   });
+// });
 
 
 /**
  * Async UI rendering events propagation prototype
  * TODO: add pubsub dynamic channeling
  */
-/*
-setInterval(function() {
-  User.create(function() {})
-}, 4000)
+// 
+// setInterval(function() {
+//   User.create(function() {})
+// }, 2000)
 
 io.sockets.on('connection', function(socket) {
   socket.emit('news', {
     hello: 'world'
   });
-  var email = null;
-  socket.on('my other event', function(data) {
-    email = data.email
-    console.log(data)
-  });
-
-  setInterval(function() {
-    if (email != null) {
-      if (emailToRenderEventsMap[email] != undefined) {
-        d = emailToRenderEventsMap[email].pop()
-        socket.emit('new-data', {
-          cell: d,
-          table: emailToRenderEventsMap[email]
-        });
-      }
-    }
-  }, 3000)
+  // var email = null;
+  // socket.on('my other event', function(data) {
+  //   email = data.email
+  //   console.log(data)
+  // });
+  // 
+  // setInterval(function() {
+  //   if (email != null) {
+  //     if (emailToRenderEventsMap[email] != undefined) {
+  //       d = emailToRenderEventsMap[email].pop()
+  //       socket.emit('new-data', {
+  //         cell: d,
+  //         table: emailToRenderEventsMap[email]
+  //       });
+  //     }
+  //   }
+  // }, 2000)
 });
-
-*/
